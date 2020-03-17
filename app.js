1232eaf4c25364b14121fadf2e72375ebaac5dd0
Benjamin <benjaminad5@yahoo.com>
2020-03-17 02:57:42 +0100

    (11 - Récupérer les paramètres du POST)

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config();
 const port = process.env.PORT
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'pug');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
   res.render('index', {title: 'Accueil'});
@@ -11,6 +14,7 @@ app.get('/', function(req, res) {
 
 app.post('/party', function(req, res) {
   res.send('Post ok !');
+  console.log(req.body);
 });
 
 app.listen(port, () => console.log(`Front app listening on port ${port}!`));
