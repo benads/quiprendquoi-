61cba8270af19bd659f6782005d1ad045f8eb25e
Benjamin <benjaminad5@yahoo.com>
2020-03-17 03:01:52 +0100

    (12 - POST vers l'API)

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv = require('dotenv').config();
 const port = process.env.PORT
 const bodyParser = require('body-parser');
+const axios = require('axios');
 
 app.set('view engine', 'pug');
 
@@ -13,8 +14,10 @@ app.get('/', function(req, res) {
 });
 
 app.post('/party', function(req, res) {
-  res.send('Post ok !');
-  console.log(req.body);
+  axios
+    .post(`${process.env.API_URL}/party`, req.body)
+    .then(({data}) => console.log(data))
+    .catch((err) => console.error(err));
 });
 
 app.listen(port, () => console.log(`Front app listening on port ${port}!`));
