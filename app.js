20f8d46ff8b90e45b5fa46b1a7784f9d90b25b70
Benjamin <benjaminad5@yahoo.com>
2020-03-18 14:12:33 +0100

    34 - Page offline

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(methodOverride('_method'));
 
 // STYLESHEET
 app.use(express.static('public'))
+app.use(express.static('pwa'))
 
 
 app.get('/', function(req, res) {
--- scripts/sw.js
+++ scripts/sw.js
@@ -4,6 +4,13 @@ addEventListener('install', (event) => {
 
 addEventListener('fetch', (event) => {
     if (event.request.headers.get('Accept').includes('text/html')) {
-        event.respondWith(fetch(event.request))
+        event.waitUntil(
+            caches.open('offline').then((cache) => {
+              cache.add('offline.html');
+            })
+          );
+        event.respondWith(
+          fetch(event.request).catch(() => caches.match('offline.html')),
+        );
     }
 });
