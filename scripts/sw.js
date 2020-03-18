a9093d02e1bbdad98057aee79dfc46ddb4f7a2ea
Benjamin <benjaminad5@yahoo.com>
2020-03-18 14:26:59 +0100

    (35 - Stocker les pages événement)

--- scripts/sw.js
+++ scripts/sw.js
@@ -2,15 +2,36 @@ addEventListener('install', (event) => {
     console.log('Hello from the service worker')
   });
 
-addEventListener('fetch', (event) => {
+  addEventListener('fetch', (event) => {
     if (event.request.headers.get('Accept').includes('text/html')) {
-        event.waitUntil(
-            caches.open('offline').then((cache) => {
-              cache.add('offline.html');
-            })
-          );
-        event.respondWith(
-          fetch(event.request).catch(() => caches.match('offline.html')),
-        );
+      event.respondWith(
+        fetch(event.request).catch(() => caches.match('offline.html')),
+        fetch(event.request)
+          .then((res) => {
+            if (isPartyPage(event.request.url)) {
+              const copy = res.clone();
+              caches
+                .open('parties')
+                .then((cache) => cache.put(event.request, copy));
+              return res;
+            } else {
+              return res;
+            }
+          })
+          .catch(() => {
+            if (isPartyPage(event.request.url)) {
+              return caches
+                .match(event.request)
+                .catch(() => caches.match('offline.html'));
+            } else {
+              return caches.match('offline.html');
+            }
+          }),
+      );
     }
-});
+  });
+
+function isPartyPage(url) {
+    return /party\/[a-zA-Z0-9]*$/.test(url);
+}
+
