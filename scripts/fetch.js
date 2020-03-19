81b342f98e43484969974daa7afbaeb8be70657b
Benjamin <benjaminad5@yahoo.com>
2020-03-19 17:07:27 +0100

    add an item if there is new item created, nedd to fix bug about last element, but fonctionnal

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -18,9 +18,25 @@ setInterval(() => {
     if(repeatCall.toString() !== localStorage.getItem('initData')) {
         localStorage.setItem('initData', repeatCall.toString());
         let item_created = response.items.length -1;
+        // Notification authorization 
+        Notification.requestPermission();
         response.items.map( (h, index) => { 
             if(index === item_created) { 
-                console.log(h.name) 
+                // Send notification if user accept
+                new Notification(`Un nouvel item à été ajouté : ${h.name}`);
+                let newDiv = document.createElement("div");
+                let form = document.createElement("form")
+                form.setAttribute('method', 'post');
+                form.setAttribute('action', `/party/5e70dd083d57b970f3812a96/items/${h._id}?_method=DELETE`)
+                let button = document.createElement("button")
+                button.innerHTML = 'Supprimez'
+                button.setAttribute('type', 'submit')
+                form.appendChild(button)
+                let newContent = document.createTextNode(h.name);
+                newDiv.appendChild(newContent)
+                newDiv.appendChild(form)
+                let currentDiv = document.getElementById('lastNotification');
+                document.body.before(newDiv, currentDiv);
             }
         })
     }})
